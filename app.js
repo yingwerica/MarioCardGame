521cf0d237ac69fd9e52c57e54c6fa31df462204
yingwerica <yingw.pro@gmail.com>
2022-12-08 21:48:52 -0700

    finish two player switch logic and winner function

--- app.js
+++ app.js
@@ -96,11 +96,12 @@ gameCards.forEach((item) => {
 )
 
 
-//////////////////////input two players name////////////////////////
+//////////////////////input and display two players name////////////////////////
 let playerName1;
 document.getElementById('nameinput1').onclick = function() {
     playerName1 = document.getElementById('text1').value 
     console.log(playerName1)
+    //display the input in a <P> element
     let playername1 = document.createElement('p')
     playername1.id = 'name1'
     let player1 = document.getElementById('player1')
@@ -109,6 +110,7 @@ document.getElementById('nameinput1').onclick = function() {
     document.getElementById('mylabel1').remove()
     document.getElementById('text1').remove()
     document.getElementById('nameinput1').remove()
+   
 }
 
 let playerName2;
@@ -125,37 +127,52 @@ document.getElementById('nameinput2').onclick = function() {
     document.getElementById('nameinput2').remove()
 }
 
-//start button, disabled after clicked
+///////////start game after valid names input, button disabled after clicked//////////
+
+let prompt = document.getElementById('prompt')
+
 document.getElementById('start').onclick = function() {
     if (document.getElementById('name1') == null || document.getElementById('name2') == null) {
-        document.getElementById('prompt').innerText = "Please enter two players' name."
+        prompt.innerText = "Please enter two players' name."
        
     } else {
-        document.getElementById('prompt').innerText = `Game start! ${playerName1}, please click on any card to find a match.`
-        player1Move()
+        prompt.innerText = `Game start! ${playerName1}, please click on any card to find a match.`
+        gameRound()
         document.getElementById('start').disabled = true
         document.getElementById('start').classList.add('disabled')
     }
 }
 
 
-////////////////////////select cards and verify if two selected are a match////////////////////////////////
 
+let currentPlayer = 1;
 let firstGuess = '';
 let secondGuess = '';
 let count = 0;
 let previousClick = null;
-let score = 0;
+let score1 = 0;
+let score2 = 0;
 let delay = 1200;
+let promptVisible = document.getElementById('prompt').style.visibility
+
 
 // Add match CSS and increment the score
-const matchOfPlayer1 = () => {
+const matchOfCards = () => {
     let selected = document.querySelectorAll('.selected')
     selected.forEach((card) => {
       card.classList.add('match')
     })
-    document.getElementById("score1").innerText = `Score: ${score}` 
+    if (currentPlayer === 1){
+    document.getElementById("score1").innerText = `Score: ${score1}`
+    } else {
+    document.getElementById("score2").innerText = `Score: ${score2}`   
+    }
+    //one round is over
+    if (document.querySelectorAll('.match').length === 24) {
+        winnerOfTheRound();
+        }
   }
+
 
 //reset the guess count after two to continue the game
 const resetCount = () => {
@@ -169,53 +186,90 @@ const resetCount = () => {
     })
 }
 
-// Add event listener to grid, and only allow two cards to be selected at a time, conditions to evalue if there is a match
-const player1Move = () => {
-    grid.addEventListener('click', function (event) {
-    // The event target is the clicked item
-    let clicked = event.target
-    // let test = document.getElementsByClassName('selected')
-    // console.log(test)
-  
-    // Do not allow the grid section itself to be selected; only select divs inside the grid
-    // and click on the same card twice are not valid
-    // and disable the fliping of already matched cards
-    if (clicked.nodeName === 'SECTION' || 
-        clicked === previousClick ||
-        clicked.parentNode.classList.contains('match')
-    ) {
-      return
+
+
+//game round is over, report the winner
+const winnerOfTheRound = () => {
+    if (score1 === score2) {
+        prompt.innerText = 'Wow!!Even steven!You both win! Refresh the page to start a new round.'
+        promptVisible = 'visible' 
+    } else if (score1 > score2) {
+        prompt.innerText = `Terrific!! ${playerName1} is the winner! Refresh the page to start a new round.`
+        promptVisible = 'visible' 
+    } else {
+        prompt.innerText = `Terrific!! ${playerName2} is the winner! Refresh the page to start a new round.`
+        promptVisible = 'visible' 
     }
-    //limit the selected cards number to 2
-    if (count < 2) {
-        count++;
-        //assign first guess and second guess after clicks
-        if (count === 1) {
-            firstGuess = clicked.parentNode.dataset.name; //each click is clicking on an inner div(front), but the name is on the outer div(card)
-            // Add selected class
-            clicked.parentNode.classList.add("selected");
-        } else {
-            secondGuess = clicked.parentNode.dataset.name;
-            // Add selected class
-            clicked.parentNode.classList.add("selected");
-        }
-         // If both guesses are not empty...
-        if (firstGuess !== '' && secondGuess !== '') {
-            // and the first guess matches the second match...
-            if (firstGuess === secondGuess) {
-                score++;
-                //add delay after selections
-                setTimeout(matchOfPlayer1, delay);
-                setTimeout(resetCount, delay);
-            }else {
-                setTimeout(resetCount, delay);
-            }
-        }
-
-    }  
-  })
 }
- 
 
+////////////////////////////game round////////////////////////////////
+// Add event listener to grid, and only allow two cards to be selected at a time, conditions to evalue if there is a match
+const gameRound = () => {
+    grid.addEventListener('click', function (event) {
+        promptVisible = 'hidden'
+        // The event target is the clicked item
+        let clicked = event.target
+        // let test = document.getElementsByClassName('selected')
+        // console.log(test)
+      
+        // Do not allow the grid section itself to be selected; only select divs inside the grid
+        // and click on the same card twice are not valid
+        // and disable the fliping of already matched cards
+        if (clicked.nodeName === 'SECTION' || 
+            clicked === previousClick ||
+            clicked.parentNode.classList.contains('match')
+            ) {
+            return
+        } 
+
+        
+        //limit the selected cards number to 2
+        if (count < 2) {
+            count++;
+            //assign first guess and second guess after clicks
+            if (count === 1) {
+                firstGuess = clicked.parentNode.dataset.name; //each click is clicking on an inner div(front), but the name is on the outer div(card)
+                // Add selected class
+                clicked.parentNode.classList.add("selected");
+            } else {
+                secondGuess = clicked.parentNode.dataset.name;
+                // Add selected class
+                clicked.parentNode.classList.add("selected");
+            }
+            // If both guesses are not empty...
+            if (firstGuess !== '' && secondGuess !== '') {
+                // if the first guess matches the second match
+                if (firstGuess === secondGuess) {
+                    if (currentPlayer === 1){
+                        score1++;
+                    }else {
+                        score2++;
+                    }
+                    prompt.innerText = 'Bravo!You found a match.Go find more!'
+                    promptVisible = 'visible'
+                   
+                    //add delay after selections
+                    setTimeout(matchOfCards, delay);
+                    setTimeout(resetCount, delay);
+                    
+                }else {
+                    //not a match, the other player's turn
+                    if (currentPlayer === 1) {
+                        prompt.innerText = `Oops!Not a match.Now it's ${playerName2}'s turn.`
+                        promptVisible = 'visible'
+                        currentPlayer = 2  
+                    } else {
+                        prompt.innerText = `Oops!Not a match.Now it's ${playerName1}'s turn.`
+                        promptVisible = 'visible'
+                        currentPlayer = 1 
+                    }; 
+                   
+                    setTimeout(resetCount, delay);    
+                }
+            }
+
+        }  
+    })
+}
 
   
